ce0d6d6453f5f36df5099a2927885bae3eb28de9
Rephobia <26533256+Rephobia@users.noreply.github.com>
2025-10-29 13:36:47 +0300

    Add one gorutine per not-premium user in 3-limit-service-time solution

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -12,6 +12,7 @@ package main
 
 import (
 	"context"
+	"sync"
 	"time"
 )
 
@@ -21,24 +22,63 @@ type User struct {
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
+	mu        sync.Mutex
 }
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
-func HandleRequest(process func(), u *User) bool {
+func HandleRequestBeginner(process func(), u *User) bool {
 	if u.IsPremium == false {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*10))
 		defer cancel()
 
-		done := make(chan struct{})
+		processDone := make(chan struct{})
 
 		go func() {
 			process()
-			close(done)
+			close(processDone)
 		}()
 
 		select {
-		case <-done:
+		case <-processDone:
+			return true
+		case <-ctx.Done():
+			return false
+		}
+	}
+
+	process()
+	return true
+}
+
+func HandleRequest(process func(), u *User) bool {
+	if u.IsPremium == false {
+		u.mu.Lock()
+		defer u.mu.Unlock()
+
+		remaining := 10 - u.TimeUsed
+		if remaining <= 0 {
+			return false
+		}
+		ctx, cancel := context.WithTimeout(
+			context.Background(),
+			time.Duration(time.Second*time.Duration(remaining)),
+		)
+
+		defer cancel()
+
+		processDone := make(chan struct{})
+
+		go func() {
+			start := time.Now()
+			process()
+			elapsed := time.Since(start)
+			u.TimeUsed += int64(elapsed.Seconds())
+			close(processDone)
+		}()
+
+		select {
+		case <-processDone:
 			return true
 		case <-ctx.Done():
 			return false
